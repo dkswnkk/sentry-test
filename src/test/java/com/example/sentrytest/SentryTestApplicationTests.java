6dc08e2b92988e61541b59fc0d1c0fb853804a26
dkswnkk <jjoy9810@gmail.com>
2023-01-14 16:44:29 +0900

    init: sentry-test

---
+++ src/test/java/com/example/sentrytest/SentryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sentrytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
